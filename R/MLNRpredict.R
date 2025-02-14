cd43005750d70c59d2252e9c79baa40ab76e0582
msalem7777 <msalem@vt.edu>
2025-02-14 10:19:10 -0500

    Added scaling

--- R/MLNRpredict.R
+++ R/MLNRpredict.R
@@ -32,8 +32,8 @@ MLNR.predict = function(dat_pred, model, cov_transform = "scale"){
   f_xi = list()
 
   # applying the above function
-  pwy_dfs_pred = pathway_creator(dat_pred, num_pwy, transform = "none")
-  pwy_dfs = pathway_creator(dat, num_pwy, transform = "none")
+  pwy_dfs = pathway_creator(dat, num_pwy, transform = "scale")
+  pwy_dfs_pred = pathway_creator_pred(dat_pred, dat, num_pwy, transform = "scale")
 
   y_hat = rep(0, (dim(dat_pred)[1]-1))
 
@@ -47,6 +47,8 @@ MLNR.predict = function(dat_pred, model, cov_transform = "scale"){
       y_hat = y_hat + Cn%*%as.matrix(alpha_mats[[i]])
     }
   }
+
+  y_hat = y_hat*sd(y) + mean(y)
 
   return(y_hat)
 
